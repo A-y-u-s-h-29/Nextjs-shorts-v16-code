298116ff1b04537b8533ec152d7518c0a8ed386d
Ayush Sharma <ayushsharma00123456@gmail.com>
2026-04-16 21:32:13 +0530

    comment added

--- src/app/page.js
+++ src/app/page.js
@@ -1,50 +1,51 @@
-// export default function NormalImages() {
-//   const images = Array.from({ length: 15 }, (_, i) => `/images/img (${i + 1}).avif`);
 
-//   return (
-//     <div>
-//       <h1>❌ Normal IMG (No Optimization)</h1>
+// ====================WITHOUT NEXT IMAGE USE=========================
 
-//       {images.map((src, index) => (
-//         <img
-//           key={index}
-//           src={src}
-//           width="300"
-//           style={{ marginBottom: "10px" }}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
-import Image from "next/image";
-
-export default function OptimizedImages() {
-  const images = Array.from({ length: 15 }, (_, i) => `/images/img (${i+1}).avif`);
+export default function NormalImages() {
+  const images = Array.from({ length: 15 }, (_, i) => `/images/img (${i + 1}).avif`);
 
   return (
     <div>
-      <h1 className="text-4xl">✅ Next Image (Optimized)</h1>
+      <h1>❌ Normal IMG (No Optimization)</h1>
 
       {images.map((src, index) => (
-        <Image
+        <img
           key={index}
           src={src}
-          alt="demo"
-          width={300}
-          height={200}
+          width="300"
           style={{ marginBottom: "10px" }}
         />
       ))}
     </div>
   );
 }
+
+
+
+
+
+// ====================WITH NEXT IMAGE USE=========================
+
+
+// import Image from "next/image";
+
+// export default function OptimizedImages() {
+//   const images = Array.from({ length: 15 }, (_, i) => `/images/img (${i+1}).avif`);
+
+//   return (
+//     <div>
+//       <h1 className="text-4xl">✅ Next Image (Optimized)</h1>
+
+//       {images.map((src, index) => (
+//         <Image
+//           key={index}
+//           src={src}
+//           alt="demo"
+//           width={300}
+//           height={200}
+//           style={{ marginBottom: "10px" }}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
